98bd3214b93d7c6f68d634d5beb7c1db830781b8
Maksym Kruk <krukmaksym@gmail.com>
2025-11-23 10:24:39 +0200

    Wiped backend.tf as not relevant for now

--- infra/terraform/global/backend/backend.tf
+++ infra/terraform/global/backend/backend.tf
@@ -1,18 +1,1 @@
-terraform {
-  backend "s3" {
-    endpoint                    = var.s3_endpoint
-    bucket                      = var.s3_bucket
-    key                         = "global/terraform.tfstate"
-    region                      = "us-east-1"
 
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    force_path_style            = true
-
-    encrypt = true
-  }
-}
-
-variable "s3_endpoint" {}
-variable "s3_bucket" {}
