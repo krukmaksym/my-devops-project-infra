cff530d473fafcbad2cc46b95087a44c0eb9bdb5
krukmaksym <krukmaksym@gmail.com>
2025-11-24 21:30:41 +0200

    [tf][network] updated module by adding lifecycle policy

--- infra/terraform/global/network/main.tf
+++ infra/terraform/global/network/main.tf
@@ -5,6 +5,7 @@ terraform {
       version = "2.69.0"
     }
   }
+  required_version = "1.5.7"
 }
 
 provider "digitalocean" {
@@ -16,4 +17,8 @@ resource "digitalocean_vpc" "vpc" {
   name     = "${var.environment}-vpc"
   region   = var.region
   ip_range = var.vpc_cidr
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
